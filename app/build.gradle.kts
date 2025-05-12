9a644700a331451fb2942581b4af23c5a3587d7f
M-Arham-Irfan <m.arham.irfan.it@gmail.com>
2025-05-12 16:23:21 +0000

    Changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.androidApplication)
+    id("com.android.application")
     id("com.google.gms.google-services")
 
 }
@@ -37,24 +37,31 @@ dependencies {
 
 
 
-
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.appcompat.v170)
+    implementation(libs.material)
+    implementation(libs.activity.v193)
+    implementation(libs.constraintlayout.v221)
+    implementation(libs.volley)
+    implementation (libs.play.services.location)
+    implementation(libs.okhttp)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
     implementation(libs.firebase.database)
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity:1.9.3")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
-    implementation("com.android.volley:volley:1.2.1")
-    implementation ("com.google.android.gms:play-services-location:21.0.1")
-    implementation("com.squareup.okhttp3:okhttp:4.9.3")
-    implementation("com.google.firebase:firebase-firestore:25.1.2")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
 
 
 
